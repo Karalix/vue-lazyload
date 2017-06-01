72c21c0a18b37b8eac2b0fa33955a2ddad05189c
Ducros Alix <ducrosalix@hotmail.fr>
2017-06-01 11:10:49 +0200

    fix 2

--- src/lazy.js
+++ src/lazy.js
@@ -44,7 +44,7 @@ export default function (Vue) {
                 let catIn = false
                 this.ListenerQueue.forEach(listener => {
                     if (listener.state.loaded) {
-                        if(listener.state.rested && !listener.checkIsNear()){ 
+                        if(!listener.state.rested && !listener.checkIsNear()){ 
                             listener.rest()
                         } else {
                             listener.unrest()
